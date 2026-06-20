d820f14c21d433f108be69e144ee535ed68c570e
Juan Manuel Orozco Ospina <juanmanuelorozcoospina@gmail.com>
2026-06-20 09:48:23 -0500

    Segunda version README

---
+++ src/sintaxis-variables/index.php
@@ -0,0 +1,13 @@
+<?php
+  define("CONSTANTE","Este valor es constante y no puede ser cambiado");
+  $nombre = "Juan";
+  $edad = 30;
+  $ciudad = "Cartago";  
+  $carrera = "Ingeniería en Sistemas";
+
+  echo "Hola, mi nombre es $nombre, tengo $edad años, vivo en $ciudad y estudio $carrera<br>";
+
+  echo 'Hola, mi nombre es $nombre, tengo $edad años, vivo en $ciudad y estudio $carrera<br>';
+
+  echo CONSTANTE."<br>";
+?>
